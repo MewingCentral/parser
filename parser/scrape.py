4c47fe3123254e15e1d159bb288757bb3258784a
anna-sage-local <anna.macinnis109@gmail.com>
2024-10-04 21:40:37 +0000

    feat: :sparkles: json output file with exam pdf paths
    scraper now outputs a json file describing the location of each exam document

--- parser/scrape.py
+++ parser/scrape.py
@@ -4,6 +4,9 @@ import requests
 from bs4 import BeautifulSoup
 import re
 import os
+import json
+
+json_output = {"documents":{}}
 
 url = "https://www.cs.ucf.edu/registration/exm/"
 read = requests.get(url) # Get request for HTML
@@ -13,6 +16,31 @@ soup = BeautifulSoup(html, "html.parser")
 # Is the tag a PDF link for an exam that has no discrete section?
 def pdf_before_discrete(tag):
     return tag.name == "a" and re.search(".pdf", tag["href"]) and not re.search("CS|DS", tag["href"])
+
+# Return a string containing the semester and year of the exam specified
+def get_semester_and_year(file_name):
+    retval = ""
+
+    # Get semester
+    if (re.search(r"(jan)|(feb)|(mar)|(apr)", file_name, re.IGNORECASE) != None):
+        retval = "spring"
+    elif (re.search(r"(may)|(jun)|(jul)", file_name, re.IGNORECASE)):
+        retval = "summer"
+    else: 
+        retval = "fall"
+
+    # Get year
+    retval += re.findall(r"[0-9]+", file_name)[0]
+    return retval
+
+# Return a string describing the type of exam document
+def get_document_type(file_name):
+    if (re.search(r"sol", file_name, re.IGNORECASE)):
+        return "solutions"
+    elif (re.search(r"info", file_name, re.IGNORECASE)):
+        return "info"
+    else:
+        return "exam"
 
 pdf_anchors = soup.find_all(pdf_before_discrete)
 
@@ -29,11 +57,27 @@ for i in range(0, len(pdf_anchors), 3):
     if (not os.path.exists(pdfs_dir + "/" + dir_name)):
         os.makedirs(pdfs_dir + "/" + dir_name)
 
+    # Configure json output
+    exam = get_semester_and_year(dir_name) + "_paths"
+    json_output["documents"][exam] = {}
+
     for doc in chunk:
         cur_link = doc.get("href")
         response = requests.get(url + cur_link)
+
         file_name = cur_link.rsplit('/', 1)[-1]
-        pdf = open(pdfs_dir + "/" + dir_name + "/" + file_name, 'wb')
+        doc_type = get_document_type(file_name)
+        path = pdfs_dir + "/" + dir_name + "/" + file_name
+
+        # Configure json output
+        json_output["documents"][exam][doc_type] = path
+
+        pdf = open(path, 'wb')
         pdf.write(response.content)
         pdf.close()
+
+output_file = open("exam_paths.json", "w")
+output_file.write(json.dumps(json_output))
+output_file.close()
+
         
