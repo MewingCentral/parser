f362e7fd0007b10997403afcbd6dc9938bb2bdec
e4s70f3d3n <avitulli@knights.ucf.edu>
2024-10-11 22:05:30 +0000

    feat: :sparkles: Add colab notebook for AI model testing and formating script

---
+++ colab-notebook/format_questions.py
@@ -0,0 +1,32 @@
+import json
+
+input_file = "data-artifacts/document.json"
+output_file = "colab-notebook/formatted questions.json"
+
+with open(input_file, "r") as f:
+    data = json.load(f)
+
+question_titles = []
+question_id = 1
+
+for section in data["sections"]:
+    for question in section["questions"]:
+        
+        question_content = question["text"]
+        formatted_prompt = {
+            "id" : question_id,
+            "content" : question_content
+        }
+
+        question_titles.append(formatted_prompt)
+        question_id += 1
+
+
+formatted_output = {
+    "questions" : question_titles
+}
+
+with open(output_file, "w") as outfile:
+    json.dump(formatted_output, outfile, indent=4)
+
+print(f"Successfully wrote question titles to {output_file}")
